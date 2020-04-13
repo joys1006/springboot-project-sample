83f3286b9a99b02cb91a99ff2d0d1566d8dd87e0
엄호철 <>
2020-04-13 23:49:39 +0900

    [ADD]  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor") 추가

--- src/main/java/com/todo/api/config/CouchBaseConnector.java
+++ src/main/java/com/todo/api/config/CouchBaseConnector.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 
 import java.util.concurrent.TimeUnit;
